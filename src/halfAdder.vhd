e73df2d17b6855bf3b9229c37f434215043d848a
sofiagen8 <c26sofia.gentile@afacademy.af.edu>
2026-01-27 19:50:35 -0700

    Working Synthesis

--- src/halfAdder.vhd
+++ src/halfAdder.vhd
@@ -9,16 +9,37 @@
 --| USAF Academy, CO 80840           \____//____/_/  |_/_/   /_/  |_|
 --| 
 --| ---------------------------------------------------------------------------
---|
+--| FILENAME      : halfAdder.vhd
+--| AUTHOR(S)     : Sofia Gentile
+--| CREATED       : 01/22/2026
 --| DESCRIPTION   : This file implements a one bit half adder.
 --|
+--| DOCUMENTATION : None
 --+----------------------------------------------------------------------------
+--| REQUIRED FILES :
 --|
---| NAMING CONVENSIONS :
+--|     Libraries : iee
+--|     Packages  : std_logic_1164, numeric_std, unisim
+--|     Files     : NONE
+--+----------------------------------------------------------------------------
+--| NAMING CONVENTIONS :
 --|
+--|    xb_<port name>           = off-chip bidirectional port ( _pads file )
+--|    xi_<port name>           = off-chip input port         ( _pads file )
+--|    xo_<port name>           = off-chip output port        ( _pads file )
+--|    b_<port name>            = on-chip bidirectional port
 --|    i_<port name>            = on-chip input port
 --|    o_<port name>            = on-chip output port
---|
+--|    c_<signal name>          = combinatorial signal
+--|    f_<signal name>          = synchronous signal
+--|    ff_<signal name>         = pipeline stage (ff_, fff_, etc.)
+--|    <signal name>_n          = active low signal
+--|    w_<signal name>          = top level wiring signal
+--|    g_<generic name>         = generic
+--|    k_<constant name>        = constant
+--|    v_<variable name>        = variable
+--|    sm_<state machine type>  = state machine type definition
+--|    s_<signal name>          = state name
 --+----------------------------------------------------------------------------
 library ieee;
   use ieee.std_logic_1164.all;
@@ -29,7 +50,8 @@ entity halfAdder is
   port(
 	i_A     : in  std_logic; -- 1-bit input port
 	i_B     : in  std_logic; 
-	o_S     : out std_logic  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
+	o_S     : out std_logic;  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
+	o_Cout  : out std_logic
 	-- TODO:  Carry port
   ); -- the semicolon is here instead
 end halfAdder;
@@ -42,6 +64,7 @@ begin
 
 	-- *concurrent* signal assignments
 	o_S    <= i_A xor i_B;
+	o_Cout <= i_A and i_B;
 	-- TODO:  Carry signal assignment
 	
 end halfAdder_arch;
